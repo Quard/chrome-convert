79fff492299bad7acaa0724f9e079aaf14eea523
Vadym Zakovinko <vp.zakovinko@gmail.com>
2011-08-27 14:15:01 +0300

    js insert to queue

--- content_script.js
+++ content_script.js
@@ -6,11 +6,28 @@ var links = {
   video: 'http://video-conversion.online-convert.com/',
   audio: 'http://audio-conversion.online-convert.com/'
 }
+var apiKey = '5d7c6b9862c6b8a9633ace4cf5fa1133';
+
+function insertToQueue(url, content_type, to_format) {
+    var request = new XMLHttpRequest();
+    var xml = '<?xml version="1.0" encoding="utf-8" ?><queue><apiKey>'+ apiKey +'</apiKey><targetType>'+ content_type +'</targetType><targetMethod>'+ to_format +'</targetMethod><testMode>true</testMode><sourceUrl>'+ escape(url) +'</sourceUrl></queue>';
+    var data = new FormData();
+    
+    data.append('queue', xml);
+    request.open("POST", 'http://api.online-convert.com/queue-insert', false);
+    request.send(data);
+
+    if (request.status == 200) {
+        var dom = request.responseXML;
+        alert(dom);
+    }
+}
 
 function convert_image(info, tab) {
    var img_url = info.linkUrl || info.srcUrl;
    var url = links.image+conversion_page+img_url;  
-   window.open(url);
+   //window.open(url);
+   insertToQueue(url);
 }
 
 function convert_ebook(info, tab) {
@@ -31,8 +48,8 @@ function convert_doc(info, tab) {
 }
 
 function convert_video(info, tab) {
-   var url = links.video+conversion_page+info.pageUrl;  
-   window.open(url);
+   var url = links.video+conversion_page+info.pageUrl;
+   insertToQueue(url, 'video', 'convert-to-avi');
 }
 
 function convert_audio(info, tab) {
