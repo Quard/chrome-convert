7cc328f516f3dd996e6e50d7eda27eb70b8db9a4
Andrew Bananos <bananos@nimble.com>
2011-08-27 12:59:46 +0300

    Some cleanup / only video-related stuff is left

--- content_script.js
+++ content_script.js
@@ -1,59 +1,56 @@
 var conversion_page='?external_url='
 var links = {
-  image: 'http://image-conversion.online-convert.com/',
-  ebook: 'http://ebook-conversion.online-convert.com/',
-  doc:   'http://document-conversion.online-convert.com/',
-  video: 'http://video-conversion.online-convert.com/',
-  audio: 'http://audio-conversion.online-convert.com/'
+ // image: 'http://image-conversion.online-convert.com/',
+ // ebook: 'http://ebook-conversion.online-convert.com/',
+//  doc:   'http://document-conversion.online-convert.com/',
+  video: 'http://video-conversion.online-convert.com/'
+//  audio: 'http://audio-conversion.online-convert.com/'
 }
+// 
+// function convert_image(info, tab) {
+//    var img_url = info.linkUrl || info.srcUrl;
+//    var url = links.image+conversion_page+img_url;  
+//    window.open(url);
+// }
 
-function convert_image(info, tab) {
-   var img_url = info.linkUrl || info.srcUrl;
-   var url = links.image+conversion_page+img_url;  
-   window.open(url);
-}
-
-function convert_ebook(info, tab) {
-   var url = links.ebook+conversion_page+info.pageUrl;  
-   window.open(url);
-}
-
-function convert_doc(info, tab) {
-   chrome.tabs.sendRequest(tab.id, {"msg": "findImages"}, function(r){
-     images = "";
-     for (img in r.images) 
-       images += r.images[img].url;
-     alert(images);
-   });
-
-   var url = links.doc+conversion_page+(info.linkUrl || info.pageUrl);  
-//   window.open(url);
-}
+// 
+// function convert_ebook(info, tab) {
+//    var url = links.ebook+conversion_page+info.pageUrl;  
+//    window.open(url);
+// }
+// 
+// function convert_doc(info, tab) {
+//    chrome.tabs.sendRequest(tab.id, {"msg": "findImages"}, function(r){
+//      images = "";
+//      for (img in r.images) 
+//        images += r.images[img].url;
+//      alert(images);
+//    });
+// 
+//    var url = links.doc+conversion_page+(info.linkUrl || info.pageUrl);  
+// }
 
 function convert_video(info, tab) {
    var url = links.video+conversion_page+info.pageUrl;  
-   window.open(url);
+	console.log("Info:" + info);
+	console.log("Tab:" + tab)
+   // window.open(url);
 }
 
-function convert_audio(info, tab) {
-   var url = links.audio+conversion_page+info.linkUrl;  
-   window.open(url);
-}
-
-var image_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_image"),
-                                             "contexts": ["image"],
-                                             "onclick": convert_image});
-
-var ebook_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_ebook"),
-                                             "contexts": ["page"],
-                                             "onclick": convert_ebook});
-
-var doc_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_doc"),
-                                           "contexts": ["page", "link"],
-                                           "onclick": convert_doc});
+// var image_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_image"),
+//                                              "contexts": ["image"],
+//                                              "onclick": convert_image});
+// 
+// var ebook_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_ebook"),
+//                                              "contexts": ["page"],
+//                                              "onclick": convert_ebook});
+// 
+// var doc_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_doc"),
+//                                            "contexts": ["page", "link"],
+//                                            "onclick": convert_doc});
 
 var video_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_video"),
-                                           "contexts": ["page"],
+                                           "contexts": ["page", "link"],
                                            "onclick": convert_video
 //                                           "documentUrlPatterns": ['*://*.youtube.com/watch*',
 //                                                                   '*://vimeo.com/*',
@@ -61,7 +58,7 @@ var video_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("co
 //                                                                   ]
                                                                    });
 
-
-var audio_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_audio"),
-                                           "contexts": ["link"],
-                                           "onclick": convert_audio});
+// 
+// var audio_menu = chrome.contextMenus.create({"title": chrome.i18n.getMessage("convert_audio"),
+//                                            "contexts": ["link"],
+//                                            "onclick": convert_audio});
