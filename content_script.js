d959ed6c807787f33da5c86b6b2773ee14ee7cb7
Vadym Zakovinko <vp.zakovinko@gmail.com>
2011-08-27 18:21:11 +0300

    working version )

--- content_script.js
+++ content_script.js
@@ -1,18 +1,26 @@
 
 function convert_video(info, tab) {
-    $.getJson('', {url: tag.pageUrl}, function (json){
-        if (json.status) {
+    $.getJSON('http://bananos.kiev.ua/gh/api.php?url='+tab.url, function (json){
+        if (json.code == 200) {
             for (var i=0; i<json.cookies.length; i++) {
                 var cookie = json.cookies[i];
                 chrome.cookies.set({
-                    url: tab.pageUrl,
+                    url: json.url,
                     domain: cookie.domain,
                     path: cookie.path,
                     name: cookie.name,
                     value: cookie.value,
-                    expirationDate: cookie.expiration,
+                    expirationDate: parseInt(cookie.expires),
                 });
             }
+            chrome.tabs.create({url: json.download_link})
+        } else {
+            var notification = webkitNotifications.createNotification(
+              'logo.png',
+              'Download video from site',
+              json.message
+            );
+            notification.show();
         }
     });
 }
